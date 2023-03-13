183d7f08bd82721e06cd6d3d6a77cb34235f0b14
ohnib <gv08100022@gmail.com>
2023-03-12 23:21:44 -0300

    Button Fixed // Catch added

--- index.js
+++ index.js
@@ -8,10 +8,19 @@ const localidadeText = document.querySelector('.localidade-text')
 const ufText = document.querySelector('.uf-text')
 const dddText = document.querySelector('.ddd-text')
 
+const findCep = document.getElementById('btn');
+findCep.addEventListener('click', btnSent);
+
+
 function cepChange(text)
 {
     cepInput = text;
     console.log(cepInput)
+}
+
+function btnSent(event) 
+{
+    event.preventDefault();
     fetch(`https://viacep.com.br/ws/${cepInput}/json`)
     .then(r => 
         {
@@ -22,7 +31,15 @@ function cepChange(text)
                     localidadeText.innerHTML = `CIDADE: ${cep.localidade}`
                     ufText.innerHTML = `UF: ${cep.uf}`
                     dddText.innerHTML = `DDD: (${cep.ddd})`
+                    findCep.innerText = 'FIND CEP'
                 })
+        },reject => 
+        {
+            findCep.innerText = 'ERROR'
+            cepText.innerHTML = `CEP:`
+            logradouroText.innerHTML = `LOGRADOURO:`
+            localidadeText.innerHTML = `CIDADE: `
+            ufText.innerHTML = `UF: `
+            dddText.innerHTML = `DDD:`
         })
-}
-
+} 
